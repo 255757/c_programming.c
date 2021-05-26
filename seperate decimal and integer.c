7e999cbc9fe13e1f996ea0a47f1639de035d0556
Akashjindal40 <84064774+255757@users.noreply.github.com>
2021-05-26 23:57:22 +0530

    Create seperate decimal and integer.c

---
+++ seperate decimal and integer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	float a,b;
+	int c;
+	printf("Enter the number: ");
+	printf("Enter the number : ");
+	scanf("%f",&a);
+	c= (int)a;
+	b=a-c;
+	printf("Integer value = %d\nDecimal value = %f",c,b);
+	c = (int)a;
+	b = a - c;
+	printf("Integer value = %d\nDecimal value = %f", c,b);
+
+	return 0;
+}
