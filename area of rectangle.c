64ad6969dabaa49e5925b5cc9f2ca79028cdcc8c
Akashjindal40 <84064774+255757@users.noreply.github.com>
2021-05-26 13:48:15 +0530

    Create area of rectangle.c

---
+++ area of rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int length,breath,area;
+	printf("enter length:");
+	scanf("%d",&length);
+	printf("enter breath:");
+	scanf("%d",&breath);
+	area = (length*breath);
+	printf("area of rectangle:%d",area);
+	return 0;
+			}
